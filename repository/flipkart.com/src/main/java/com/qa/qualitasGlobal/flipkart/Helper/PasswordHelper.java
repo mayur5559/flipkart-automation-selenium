023f53d9062af16442a8c5d442969c62e754f600
Mayur Adhude <mayur.adhude@gmail.com>
2019-01-13 23:36:32 +0530

    Update PasswordHelper.java
    Extends TestBase.java Class

--- repository/flipkart.com/src/main/java/com/qa/qualitasGlobal/flipkart/Helper/PasswordHelper.java
+++ repository/flipkart.com/src/main/java/com/qa/qualitasGlobal/flipkart/Helper/PasswordHelper.java
@@ -10,12 +10,13 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
+import com.qa.qualitasGlobal.flipkart.base.TestBase;
 import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 /*PasswordUtils - Encrypt & Decrypt the password */
 
 
-public class PasswordHelper {
+public class PasswordHelper extends TestBase {
 
 	private static final String ALGO = "AES";
 	private static final byte[] keyValue = new byte[] { 'T', 'h', 'e', 'B', 'e', 's', 't', 'S', 'e', 'c', 'r', 'e', 't',
